0491f1085241f6222ac3b9d7cb5172b9c9ca41bd
Ghulam-Mustfa <mustafanagori89@gmail.com>
2023-05-28 07:02:22 -0700

    add comment at login screen.

--- lib/login.dart
+++ lib/login.dart
@@ -5,6 +5,7 @@ import 'package:doctorandpatient/splashscreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+//
 import 'core/utils.dart';
 import 'core/size_configuration.dart';
 
