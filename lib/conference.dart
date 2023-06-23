a0bb85b4eff7fa450a6233d39e708a49dafc3d50
Ghulam-Mustfa <mustafanagori89@gmail.com>
2023-06-22 23:21:14 -0700

    add file and correct tht cards

---
+++ lib/conference.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Conference extends StatelessWidget {
+  const Conference({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Conference RoOM"),
+      ),
+    );
+  }
+}
